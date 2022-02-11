d360209edceb47efa70c5708d8cdf75b68eca412
Keith O'Rourke <ryan_orourke@live.com>
2022-02-11 15:15:58 -0500

    Added interval based animation for page header

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,12 +1,23 @@
 import { useState, useEffect } from "react"
 
 const HomePage = () => {
+  let newString = ""
   const animatedString = "Hello!"
   const [stringFrame, setStringFrame] = useState("")
 
   useEffect(() => {
-    
+    let i=0
+    const titleAnimation = setInterval(() => {
+      newString += animatedString[i]
+      console.log("Hello")
+      setStringFrame(newString)
+      i++
+      if(i >=animatedString.length) {
+        clearInterval(titleAnimation)
+      }
+    }, 50)
   }, [])
+
   return (
     <section className="home">
       <h1 className="home__title">{stringFrame}</h1>
