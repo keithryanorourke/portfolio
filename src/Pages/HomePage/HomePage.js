0acb3c2361e97e6f98e7af31fa72d3b7cb06ac20
Keith O'Rourke <ryan_orourke@live.com>
2022-02-11 16:03:12 -0500

    Restructured animString function to accept an initialState and to return an object containing the end result string and the total time the animation takes to complete

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,27 +1,40 @@
 import { useState, useEffect } from "react"
+import "./HomePage.scss"
 
 const HomePage = () => {
-  let newString = ""
-  const animatedString = "Hello!"
-  const [stringFrame, setStringFrame] = useState("")
+  const timeOutArray = []
+  const [titleText, setTitleText] = useState("")
+  const [bodyText, setBodyText] = useState("")
+
+  const animateString = (initialState, content, setter, interval) => {
+    console.log("Start:", initialState)
+    let newString = initialState
+    let i = 0
+    const stringAnimation = setInterval(() => {
+      newString += content[i]
+      setter(newString)
+      i++
+      if(i >=content.length) {
+        clearInterval(stringAnimation)
+      }
+    }, interval)
+    return {newContent: initialState + content, time: interval*content.length}
+  }
 
   useEffect(() => {
-    let i=0
-    const titleAnimation = setInterval(() => {
-      newString += animatedString[i]
-      console.log("Hello")
-      setStringFrame(newString)
-      i++
-      if(i >=animatedString.length) {
-        clearInterval(titleAnimation)
-      }
-    }, 50)
+    const title = animateString("", "Hello!", setTitleText, 50)
+    timeOutArray.push(setTimeout(() => {
+      const firstSentence = animateString("", "My name is Keith Ryan O'Rourke and I'm a full stack web developer!", setBodyText, 20)
+      timeOutArray.push(setTimeout(() => {
+        animateString(firstSentence.newContent, " I love collaboration and I'm very passionate about education and professional growth!", setBodyText, 20)
+      }, firstSentence.time + 500))
+    }, title.time + 150))
   }, [])
 
   return (
     <section className="home">
-      <h1 className="home__title">{stringFrame}</h1>
-      <p className="home__copy">My name is Keith Ryan O'Rourke and I'm a full stack web developer!</p>
+      <h1 className="home__title">{titleText}</h1>
+      <p className="home__copy">{bodyText}</p>
     </section>
   )
 }
