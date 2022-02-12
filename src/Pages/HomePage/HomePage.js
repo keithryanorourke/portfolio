94537f2373948fce095bcd0bee8eab60522834eb
Keith O'Rourke <ryan_orourke@live.com>
2022-02-11 21:40:12 -0500

    Created placeholder ProjectsSection

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react"
 import "./HomePage.scss"
+import ProjectsSection from "../../components/ProjectsSection/Projectssection"
 
 const HomePage = () => {
   const timeOutArray = []
@@ -67,58 +68,61 @@ const HomePage = () => {
   }, [])
 
   return (
-    <section className="home">
-      {!showText ? 
-      <form onSubmit={submitHandler} className="home__codebox">
-        <label className="home__code-label"><span className="home__declaration">const</span> myName = <div className="home__input-wrapper">"
-          <input 
-            type="text" 
-            style={nameInput.style} 
-            onChange={(e) => inputChangeHandler(e, setNameInput)} 
-            name="myName" 
-            value={nameInput.value} 
-            className="home__input" 
-            />
-        "</div></label>
-        <label className="home__code-label"><span className="home__declaration">const</span> type = <div className="home__input-wrapper">"
-          <input 
-            type="text" 
-            style={developerInput.style} 
-            onChange={(e) => inputChangeHandler(e, setDeveloperInput)} 
-            name="developer" 
-            value={developerInput.value} 
-            className="home__input" 
-            />
-        "</div></label>
-        <label className="home__code-label"><span className="home__declaration">const</span> lovesCollaboration = <div className="home__input-wrapper">
-          <input 
-            type="text" 
-            style={loveInput.style} 
-            onChange={(e) => inputChangeHandler(e, setLoveInput)} 
-            name="love" 
-            value={loveInput.value} 
-            className="home__input" 
-            />
-        </div></label>
-        <div className="home__myPortfolio-container">
-          <span className="home__code"><span className="home__declaration">const</span><span className="home__function"> myPortfolio</span>{" = (nameStr, typeStr, bool) => {"}</span>
-          <span className="home__code home__code--single-indent"><span className="home__declaration">if</span>{"(!bool) {"}</span>
-          <span className="home__code home__code--double-indent">alert('Lies!!')</span>
-          <span className="home__code home__code--single-indent">{"}"}<span className="home__declaration"> else</span> {"{"}</span>
-          <span className="home__code home__code--double-indent">console.<span className="home__function">log</span>{"(`My name is ${nameStr} and I am a ${typeStr} web developer!`)"}</span>
-          <span className="home__code home__code--single-indent">{"}"}</span>
-          <span className="home__code">{"}"}</span>
-        </div>
-        <span className="home__code"><span className="home__function">myPortfolio</span>(myName, type, lovesCollaboration)</span>
-        <button className="home__submit">RUN</button>
-      </form>
-      : 
-      <>
-        <h1 className="home__title">{titleText}{textCursor === 'title' ? <div className="home__text-cursor"></div> : ""}</h1>
-        <p className="home__copy">{bodyText}{textCursor === 'p1' ? <div className="home__text-cursor"></div> : ""}</p>
-      </>
-      }
-    </section>
+    <>
+      <section className="home">
+        {!showText ? 
+        <form onSubmit={submitHandler} className="home__codebox">
+          <label className="home__code-label"><span className="home__declaration">const</span> myName = <div className="home__input-wrapper">"
+            <input 
+              type="text" 
+              style={nameInput.style} 
+              onChange={(e) => inputChangeHandler(e, setNameInput)} 
+              name="myName" 
+              value={nameInput.value} 
+              className="home__input" 
+              />
+          "</div></label>
+          <label className="home__code-label"><span className="home__declaration">const</span> type = <div className="home__input-wrapper">"
+            <input 
+              type="text" 
+              style={developerInput.style} 
+              onChange={(e) => inputChangeHandler(e, setDeveloperInput)} 
+              name="developer" 
+              value={developerInput.value} 
+              className="home__input" 
+              />
+          "</div></label>
+          <label className="home__code-label"><span className="home__declaration">const</span> lovesCollaboration = <div className="home__input-wrapper">
+            <input 
+              type="text" 
+              style={loveInput.style} 
+              onChange={(e) => inputChangeHandler(e, setLoveInput)} 
+              name="love" 
+              value={loveInput.value} 
+              className="home__input" 
+              />
+          </div></label>
+          <div className="home__myPortfolio-container">
+            <span className="home__code"><span className="home__declaration">const</span><span className="home__function"> myPortfolio</span>{" = (nameStr, typeStr, bool) => {"}</span>
+            <span className="home__code home__code--single-indent"><span className="home__declaration">if</span>{"(!bool) {"}</span>
+            <span className="home__code home__code--double-indent">alert('Lies!!')</span>
+            <span className="home__code home__code--single-indent">{"}"}<span className="home__declaration"> else</span> {"{"}</span>
+            <span className="home__code home__code--double-indent">console.<span className="home__function">log</span>{"(`My name is ${nameStr} and I am a ${typeStr} web developer!`)"}</span>
+            <span className="home__code home__code--single-indent">{"}"}</span>
+            <span className="home__code">{"}"}</span>
+          </div>
+          <span className="home__code"><span className="home__function">myPortfolio</span>(myName, type, lovesCollaboration)</span>
+          <button className="home__submit">RUN</button>
+        </form>
+        : 
+        <>
+          <h1 className="home__title">{titleText}{textCursor === 'title' ? <div className="home__text-cursor"></div> : ""}</h1>
+          <p className="home__copy">{bodyText}{textCursor === 'p1' ? <div className="home__text-cursor"></div> : ""}</p>
+        </>
+        }
+      </section>
+      <ProjectsSection />
+    </>
   )
 }
 
---
+++ src/components/ProjectsSection/Projectssection.js
@@ -0,0 +1,11 @@
+import './ProjectsSection.scss'
+
+const ProjectsSection = () => {
+  return (
+    <section className="projects">
+      <h2 className="projects__title">Projects</h2>
+    </section>
+  )
+}
+
+export default ProjectsSection
