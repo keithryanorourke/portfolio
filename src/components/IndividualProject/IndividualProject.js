d7aa11c5b14031633a3bbddf26eca6478a05c4d3
Keith O'Rourke <ryan_orourke@live.com>
2022-02-12 14:01:08 -0500

    Wrapped IndividualProject elements in an anchor tag to send visitors to project website

--- src/components/IndividualProject/IndividualProject.js
+++ src/components/IndividualProject/IndividualProject.js
@@ -4,9 +4,11 @@ import {NavLink} from 'react-router-dom'
 const IndividualProject = ({thumbnail, link, name, description}) => {
   return (
     <article className="single-project">
+      <a href={link} target="_blank" className="single-project__link">
       <img src={thumbnail} alt="" className="single-project__thumbnail" />
       <h3 className="single-project__title">{name}</h3>
       <p className="single-project__copy">{description}</p>
+      </a>
     </article>
   )
 }
