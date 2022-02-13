8f94dec385f5b96b35f4e1369caa9225c33f9d87
Keith O'Rourke <ryan_orourke@live.com>
2022-02-13 00:41:19 -0500

    Changed header bg to linear-gradient, added bg color to contact section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.scss';
 import HomePage from './Pages/HomePage/HomePage';
+import background1 from "./assets/images/body_background.jpg"
+import background2 from "./assets/images/body_background2.jpg"
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <div className="app__background-overlay"></div>
+      <img src={background1} alt="" className="app__background" />
+      <img src={background2} alt="" className="app__background app__background--wide" />
         <HomePage/>
     </div>
   );
