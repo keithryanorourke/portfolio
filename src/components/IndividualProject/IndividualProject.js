0e773083a5c3396c83989628aba144bc8d1acc9f
Keith O'Rourke <ryan_orourke@live.com>
2022-02-13 13:22:30 -0500

    Cleared all eslint warnings

--- src/components/IndividualProject/IndividualProject.js
+++ src/components/IndividualProject/IndividualProject.js
@@ -1,10 +1,9 @@
 import "./IndividualProject.scss"
-import {NavLink} from 'react-router-dom'
 
 const IndividualProject = ({thumbnail, link, name, description}) => {
   return (
     <article className="single-project">
-      <a href={link} target="_blank" className="single-project__link">
+      <a href={link} rel='noreferrer' target="_blank" className="single-project__link">
         <img src={thumbnail} alt="" className="single-project__thumbnail" />
         <h3 className="single-project__title">{name}</h3>
         <p className="single-project__copy">{description}</p>
