788b0ff10484005c1260ac4f60c5a38358e01336
Keith O'Rourke <ryan_orourke@live.com>
2022-04-10 15:51:39 -0400

    made projectsArray filled with JSX elements to render to ProjectsSection

--- src/components/ProjectsSection/ProjectsSection.js
+++ src/components/ProjectsSection/ProjectsSection.js
@@ -1,23 +1,50 @@
-import './ProjectsSection.scss'
-import liftBookThumbnail from "../../assets/images/liftBookCapture.png"
-import IndividualProject from '../IndividualProject/IndividualProject'
-import SectionTitle from '../SectionTitle/SectionTitle'
+import "./ProjectsSection.scss";
+import liftBookThumbnail from "../../assets/images/liftBookCapture.png";
+import shapeMakerthumbnail from "../../assets/images/ShapeMakerCapture.jpg"
+import IndividualProject from "../IndividualProject/IndividualProject";
+import SectionTitle from "../SectionTitle/SectionTitle";
 
 const ProjectsSection = () => {
-  return (
-    <section id="projects" className="projects">
-      <SectionTitle title='PROJECTS' />
-      <div className="projects__container">
-        <IndividualProject
-          thumbnail={liftBookThumbnail}
-          link='https://liftbook.ca'
-          name='liftBook'
-          description="liftBook is a full stack workout tracker built primarily using React, Express, and MySQL!"
-        />
-      </div>
-    </section>
-    
-  )
-}
+	// class Project {
+	// 	constructor(thumbnail, link, name, description, github, stack) {
+	// 		this.thumbnail = thumbnail;
+	// 		this.link = link;
+	// 		this.name = name;
+	// 		this.description = description;
+	// 		this.github = github;
+	// 		this.stack = stack;
+	// 	}
 
-export default ProjectsSection
+	// 	render() {
+	// 		return <IndividualProject thumbnail={this.thumbnail} link={this.link} name={this.name} description ={this.description} />;
+	// 	}
+	// }
+
+	const projectsArray = [
+		<IndividualProject
+			thumbnail={liftBookThumbnail}
+			link="https://liftbook.ca"
+			name="liftBook"
+			description="liftBook is a full stack workout tracker built primarily using React, Express, and MySQL!"
+			key="liftbook"
+		/>,
+		<IndividualProject
+			thumbnail={shapeMakerthumbnail}
+			link="https://shapemaker.app"
+			name="ShapeMaker"
+			description="ShapeMaker is a fun, relaxing, turn your brain off and just make some shapes drawing app!"
+			key="liftbook"
+		/>,
+	];
+
+	return (
+		<section id="projects" className="projects">
+			<SectionTitle title="PROJECTS" />
+			<div className="projects__container">
+				{projectsArray.map((project) => project)}
+			</div>
+		</section>
+	);
+};
+
+export default ProjectsSection;
