6d3e5a359645d620e5e031b72aa7f95591ab14c6
Keith O'Rourke <ryan_orourke@live.com>
2022-04-10 16:24:25 -0400

    Updated responsive design for contact section

--- src/components/IndividualProject/IndividualProject.js
+++ src/components/IndividualProject/IndividualProject.js
@@ -4,8 +4,8 @@ const IndividualProject = ({thumbnail, link, name, description}) => {
   return (
     <article className="single-project">
       <a href={link} rel='noreferrer' target="_blank" className="single-project__link">
-        <img src={thumbnail} alt="" className="single-project__thumbnail" />
         <h3 className="single-project__title">{name}</h3>
+        <img src={thumbnail} alt="" className="single-project__thumbnail" />
         <p className="single-project__copy">{description}</p>
       </a>
     </article>
--- src/components/ProjectsSection/ProjectsSection.js
+++ src/components/ProjectsSection/ProjectsSection.js
@@ -1,30 +1,16 @@
 import "./ProjectsSection.scss";
 import liftBookThumbnail from "../../assets/images/liftBookCapture.png";
-import shapeMakerthumbnail from "../../assets/images/ShapeMakerCapture.jpg"
+import shapeMakerthumbnail from "../../assets/images/ShapeMakerCapture.png";
 import IndividualProject from "../IndividualProject/IndividualProject";
 import SectionTitle from "../SectionTitle/SectionTitle";
 
 const ProjectsSection = () => {
-	// class Project {
-	// 	constructor(thumbnail, link, name, description, github, stack) {
-	// 		this.thumbnail = thumbnail;
-	// 		this.link = link;
-	// 		this.name = name;
-	// 		this.description = description;
-	// 		this.github = github;
-	// 		this.stack = stack;
-	// 	}
-
-	// 	render() {
-	// 		return <IndividualProject thumbnail={this.thumbnail} link={this.link} name={this.name} description ={this.description} />;
-	// 	}
-	// }
-
 	const projectsArray = [
 		<IndividualProject
 			thumbnail={liftBookThumbnail}
 			link="https://liftbook.ca"
 			name="liftBook"
+			badge="Full Stack"
 			description="liftBook is a full stack workout tracker built primarily using React, Express, and MySQL!"
 			key="liftbook"
 		/>,
@@ -32,8 +18,10 @@ const ProjectsSection = () => {
 			thumbnail={shapeMakerthumbnail}
 			link="https://shapemaker.app"
 			name="ShapeMaker"
+			badge="Front End"
 			description="ShapeMaker is a fun, relaxing, turn your brain off and just make some shapes drawing app!"
 			key="liftbook"
+			last="true"
 		/>,
 	];
 
